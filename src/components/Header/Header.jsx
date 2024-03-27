ba6a5c88cf5ee6f03c0567f12355e9694e385df3
Leia <leiaooi.94@gmail.com>
2024-03-26 21:03:14 -0700

    Add jsx to header, no styling yet.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,29 @@
+import brainflix from "../../assets/logo/BrainFlix-logo.svg";
+import search from "../../assets/icons/search.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import upload from "../../assets/icons/upload.svg";
+
+import "./Header.scss";
+
+
 function Header () {
 	return (
 		<header>
-			<h1>My React App</h1>
+			<nav className="nav">
+				<img className="nav-logo" src={brainflix} alt="Brainflix icon" />
+				<div className="nav-bar">
+					<input className="nav-bar__search" type="text" placeholder="Search" />
+					<img className="nav-bar__icon" src={search} alt="search icon" />
+					<img className="nav-avatar" src={avatar} alt="avatar"/>
+				</div>
+
+				<div className="nav-upload">
+					<button className="nav-button">UPLOAD</button>
+						<img src={upload} alt="upload"/>
+				</div>
+
+			
+			</nav>
 		</header>
 	);
 }
